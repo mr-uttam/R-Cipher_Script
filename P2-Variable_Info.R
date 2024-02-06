4eef342fb3e8fbdc52c71d9f37da09f52f9c2a7a
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-02-06 18:16:22 +0530

    checking variable information in R, including class and data type.

--- P2-Variable_Info.R
+++ P2-Variable_Info.R
@@ -1,3 +1,7 @@
 first<-'Uttam'
 class(first)
-typeof(first)
+typeof(first) ## its basically gives us a character data type
+## X <- 100
+## typeof(X)   its gives us Double not integer data type
+## Y <- 100L
+## typeof(X)   its gives us integer data type
