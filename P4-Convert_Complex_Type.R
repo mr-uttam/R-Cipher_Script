bd716af5b8a617b2b609e1214b59cfbc6221f9ed
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-02-11 01:00:57 +0530

    converting complex numbers to different data types in R.

---
+++ P4-Convert_Complex_Type.R
@@ -0,0 +1,18 @@
+num1 <- as.numeric(76 + 4i)
+print("Converted complex to numerical:")
+print(num1)
+
+# Converting complex to character
+num2 <- as.character(76 + 4i)
+print("Converted complex to character:")
+print(num2)
+
+# Converting complex to logical
+num3 <- as.logical(76 + 4i)
+print("Converted complex to logical:")
+print(num3)
+
+# Converting complex to integer
+num4 <- as.integer(76 + 4i)
+print("Converted complex to integer:")
+print(num4)
