b80ac5a4dd77ce03426c83536684835d947454d9
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-02-11 01:08:55 +0530

    converting logical values to different data types in R.

---
+++ P05-Convert_Logical_Type.R
@@ -0,0 +1,18 @@
+num1 <- as.numeric(TRUE)
+print("Converted logical to numerical:")
+print(num1)
+
+# Converting logical to character
+num2 <- as.character(TRUE)
+print("Converted logical to character:")
+print(num2)
+
+# Converting logical to integer
+num3 <- as.integer(TRUE)
+print("Converted logical to integer:")
+print(num3)
+
+# Converting logical to complex
+num4 <- as.complex(TRUE)
+print("Converted logical to complex:")
+print(num4)
