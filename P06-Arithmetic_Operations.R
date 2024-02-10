15074c4b398c19c98b7abea6b3879a83dd63871c
Uttam Pal <152017733+mr-uttam@users.noreply.github.com>
2024-02-11 01:10:06 +0530

    basic arithmetic operations in R with two integer numbers.

---
+++ P06-Arithmetic_Operations.R
@@ -0,0 +1,14 @@
+x <- 45L
+y <- 34L
+
+# Performing arithmetic operations
+result1 <- x + y
+result2 <- x - y
+result3 <- x * y
+result4 <- x / y
+
+# Printing the results
+print(paste("Addition of two numbers is:", result1))
+print(paste("Subtraction of two numbers is:", result2))
+print(paste("Multiplication of two numbers is:", result3))
+print(paste("Division of two numbers is:", result4))
